b5879d5b50db57a3bc1cb0705f8cba4a00d76d27
Scglynn <53241290+Scglynn@users.noreply.github.com>
2021-09-26 17:14:03 -0500

    Script work

---
+++ Hour 6 TIY/Assets/Script/Loop.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Loop : MonoBehaviour
+{
+    public int sum;
+    int count = 499;
+    void Start()
+    {
+        while(count <= 499 || count != 2)
+        {
+            if(count % 2 == 0)
+            {
+                sum += count;
+            }
+            count--;
+        }
+        print(sum);
+    }
+}
