87a9c92094c9fd421fcd7a5005436ff13df08e1c
Scglynn <53241290+Scglynn@users.noreply.github.com>
2021-10-20 15:09:10 -0500

    next two chapters added

---
+++ Chaos Ball/Assets/Scripts/GoalScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public bool isSolved = false;
+    void OnTriggerEnter(Collider collider)
+    {
+        GameObject collidedWith = collider.gameObject;
+        if (collidedWith.tag == gameObject.tag)
+        {
+            isSolved = true;
+            GetComponent<Light>().enabled = false;
+            Destroy(collidedWith);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Hour 9 TIY/Assets/Scripts/MotionScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionScript : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hVal = Input.GetAxis("Horizontal");
+        float vVal = Input.GetAxis("Vertical");
+
+        if (hVal != 0)
+            transform.Translate(hVal, 0, 0);
+        if (vVal != 0)
+            transform.Translate(0, vVal, 0);
+        
+    }
+}
